06046f2c777525f96e820e9c472a8b6141ef7560
unknown <kelvimanavadaria17012003@gmail.com>
2021-09-15 01:55:11 +0530

    INDEX PAGE SHOWING TASKS

--- add_task.php
+++ add_task.php
@@ -14,6 +14,20 @@ include("config/constants.php");
     <h1>Tasks Manager</h1>
     <a href="<?php echo LINK?>index.php">HOME</a>
     <h3>ADD TASK PAGE</h3>
+    <!-- TO DISPLAY MESSAGE -->
+    <p>
+            <?php
+                if(isset($_SESSION['add_fail_task']))
+                {
+                    // display message for fail
+                    echo $_SESSION['add_fail_task'];
+                    //session end
+                    unset($_SESSION['add_fail_task']);
+                }
+               
+            ?>
+            
+        </p>
     <form action="" method="POST">
         <table>
             <tr>
@@ -27,48 +41,48 @@ include("config/constants.php");
              <tr>
                  <td>SELECT LIST</td>
                  <td>
-                
-
-                     <select name="list_options">
+                 
+                     <select name="list_options" id="">
+                    
                          
-                         <?php
+                     <?php
                             
-                             $db = mysqli_connect(LOCALHOST,DB_USERNAME,DB_PASSWORD) or die("ERROR OCCURED");
-                             $db_connect=mysqli_select_db($db,DB_TABLENAME);
-                             $sql = "SELECT * FROM tdl_list";
-                             $result = mysqli_query($db,$sql);
-                             
-                             if($result==true)
-                             {
-                                   // count number of rows
-                                   $count_rows = mysqli_count_rows($db);
-                                   if($count_rows>0)
-                                   {
-                                        // display all rows
-                                        while($row = mysqli_fetch_assoc($db))
-                                        {
-                                           print_r($row);
-                                            $list_id = $row['list_id'];
-                                            $list_name = $row['list_name'];
-                                            ?>
-                                                <option value="<?php echo $list_id?>"><?php echo $list_name?></option>
-                                                   
-                                            <?php
-                                        }
-
-                                   }
-                                   else
-                                   {
-                                       //display none as option
-
-                                       ?>
-                                       <option value="0">NONE</option>
-                                       <?php
-                                   }
-                             }
+                            $db = mysqli_connect(LOCALHOST,DB_USERNAME,DB_PASSWORD) or die("ERROR OCCURED");
+                            $db_connect=mysqli_select_db($db,DB_TABLENAME);
+                            $sql = "SELECT * FROM tdl_list";
+                            $result = mysqli_query($db,$sql);
                             
-                         ?>
-                         
+                            if($result==true)
+                            {
+                                  // count number of rows
+                                  $count_rows = mysqli_count_rows($db);
+                                  if($count_rows>0)
+                                  {
+                                       // display all rows
+                                       while($row = mysqli_fetch_assoc($db))
+                                       {
+                                          print_r($row);
+                                           $list_id = $row['list_id'];
+                                           $list_name = $row['list_name'];
+                                           ?>
+                                               <option value="<?php echo $list_id?>"><?php echo $list_name?></option>
+                                                  
+                                           <?php
+                                       }
+
+                                  }
+                                  else
+                                  {
+                                      //display none as option
+
+                                      ?>
+                                      <option value="0">NONE</option>
+                                      <?php
+                                  }
+                            }
+                           
+                        ?>
+                        <option value="0"></option> 
                      </select>
                      
                  </td>
@@ -76,7 +90,7 @@ include("config/constants.php");
              <tr>
                  <td>PRIOPRITY</td>
                  <td>
-                     <select name="" id="">
+                     <select name="priority" id="">
                          <option value="high">HIGH</option>
                          <option value="medium">MEDIUM</option>
                          <option value="low">LOW</option>
@@ -94,3 +108,52 @@ include("config/constants.php");
     </form>
 </body>
 </html>
+
+
+
+<?php
+
+if(isset($_POST['submit']))
+{
+    $task_name = $_POST['task_name'];
+    $task_description=$_POST['task_description'];
+    $list_id=$_POST['list_options'];
+    $priority = $_POST['priority'];
+    $deadline = $_POST['deadline'];
+    //connect database
+    $conn2 = mysqli_connect(LOCALHOST,DB_USERNAME,DB_PASSWORD) or die("error");
+    //select database
+    $select2=mysqli_select_db($conn2,DB_TABLENAME) or die("error");
+
+    $sql2="INSERT INTO tdl_task (task_name,task_description,list_id,priority,deadline) VALUES ('$task_name','$task_description','$list_options','$priority','$deadline')";
+    
+    $executee = mysqli_query($conn2,$sql2);
+
+    if($executee==true)
+    {
+        $_SESSION['add_task']="LIST ADDED SUCCESSFULLY";
+        
+        // redirect to home page
+        header('LOCATION:'.LINK."index.php");
+       
+
+    }
+    else
+    {
+         // create a session to dispaly message
+         $_SESSION['add_fail_task']="LIST FAILED TO ADDED";
+         // redirect to same page
+         header('LOCATION:'. LINK."add_task.php");
+    }
+
+    
+
+
+
+
+
+
+    
+}
+
+?>
--- index.php
+++ index.php
@@ -21,6 +21,22 @@ include("config/constants.php");
 </div>
     <br>
     <br>
+    <!-- TO DISPLAY MESSAGE -->
+    <p>
+            <?php
+                if(isset($_SESSION['add_task']))
+                {
+                    // display message 
+                    echo $_SESSION['add_task'];
+                    //session end
+                    unset($_SESSION['add_task']);
+                }
+               
+            ?>
+            
+        </p>
+        <br>
+        <br>
     <a href="<?php echo LINK?>add_task.php">ADD TASK</a>
     <br>
     <br>
@@ -35,6 +51,61 @@ include("config/constants.php");
     <td>ACTION</td>
 
     </tr>
+    <?php
+          $db = mysqli_connect(LOCALHOST,DB_USERNAME,DB_PASSWORD) or die("ERROR OCCURED");
+          $db_connect=mysqli_select_db($db,DB_TABLENAME);
+          $sql = "SELECT * FROM tdl_tasks";
+          $result = mysqli_query($db,$sql);
+          if($result==true)
+          {
+            $sr_variable=1;
+            $count_row = mysqli_num_rows($result);
+            if($count_row>0)
+            {
+                       while($rows=mysqli_fetch_assoc($result))
+                       {
+                            $task_id = $rows['task_id'];
+                            $task_name = $rows['task_name'];
+                            $task_description = $rows['task_description'];
+                            $list_id	 = $rows['list_id	'];
+                            $priority = $rows['priority'];
+                            $deadline = $rows['deadline'];
+                            ?>
+                            <tr>
+                                 <td><?php echo $sr_variable++ ?></td>
+
+                                <td><?php echo $task_name?></td>
+                                <td><?php echo $priority  ?></td>
+                                <td><?php echo $deadline ?></td>
+
+
+                                <td>
+                                    <a href="#">UPDATE</a>
+                                    <a href="#">DELETE</a>
+
+                                </td>
+                            </tr>
+                            <?php
+
+
+
+                       }
+            }
+            else
+            {
+                ?>
+                    <tr>
+                        <td>NO TASK ADDED</td>
+                    </tr>
+
+
+
+                <?php
+            }
+          }
+    
+    
+    ?>
     <tr>
     <td>1.</td>
     <td>DESIGN A WEBSITE</td>
